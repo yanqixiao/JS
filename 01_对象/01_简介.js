bb1ff58a03501d3f9a2574cbf09b81138ba5dce8
yanqixiao <yanqixiao@163.com>
2022-11-16 05:57:32 +0800

    cq

---
+++ 01_对象/01_简介.js
@@ -0,0 +1,12 @@
+/*
+*@File    :   01_简介.js
+*@Time    :   2022/11/16 05:57:21
+*@Author  :   Yqx
+*@Version :   1.0
+*@Contact :   yanqixiao@163.com
+*@License :   (C)Copyright 2020-2120, OLE Technology Co., Ltd
+*@Desc    :   None
+*/
+
+// here put the import lib
+
